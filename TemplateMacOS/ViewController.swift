837e116fa30a9b13540b319933dd9b63eeb7c9c0
Peter Rogers <p.rogers@gold.ac.uk>
2017-04-29 21:56:27 +0100

    added displayLink

--- TemplateMacOS/ViewController.swift
+++ TemplateMacOS/ViewController.swift
@@ -9,6 +9,9 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    
+    var displayLink: CVDisplayLink?
+     public static var animationQueue: DispatchQueue = DispatchQueue.main
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +24,28 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
+    
+    private func createDisplayLink() {
+        let error = CVDisplayLinkCreateWithActiveCGDisplays(&displayLink)
+        guard let dLink = displayLink, kCVReturnSuccess == error else {
+           // N//SLog("Display Link created with error: %d", error)
+            self.displayLink = nil
+            return
+        }
+        
+        /// nowTime is the current frame time, and outputTime is when the frame will be displayed.
+        CVDisplayLinkSetOutputHandler(dLink) { (_, nowTime, outputTime, _, _) in
+            let fps = (outputTime.pointee.rateScalar * Double(outputTime.pointee.videoTimeScale) / Double(outputTime.pointee.videoRefreshPeriod))
+            ViewController.animationQueue.async {
+                //print("foof")
+                print(fps)
+                //self.checkPlayerLevels()
+            }
+            return kCVReturnSuccess
+        }
+        CVDisplayLinkStart(dLink)
+    }
+
 
 
 }
