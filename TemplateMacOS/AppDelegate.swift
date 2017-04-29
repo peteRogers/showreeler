999ef6a51a277224c7c6e5dbd377d453befc38a3
Peter Rogers <p.rogers@gold.ac.uk>
2017-04-29 21:43:19 +0100

    Initial Commit

---
+++ TemplateMacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TemplateMacOS
+//
+//  Created by Peter Rogers on 29/04/2017.
+//  Copyright © 2017 swim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TemplateMacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TemplateMacOS
+//
+//  Created by Peter Rogers on 29/04/2017.
+//  Copyright © 2017 swim. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
